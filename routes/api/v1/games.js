4e47eb2f2ecf30c74f181438c95cf479fbd114c7
Tim Allen <tnelsallen3@gmail.com>
2019-04-26 12:48:46 -0600

    Adds update and delete

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -47,4 +47,45 @@ router.post("/", function(req, res, next) {
     });
 });
 
+router.put("/:id", function (req, res, next) {
+  Game.update(
+    {
+      title: req.body.title,
+      price: req.body.price,
+      releaseYear: req.body.releaseYear,
+      active: req.body.active
+    },
+    {
+      returning: true,
+      where: {
+        id: parseInt(req.params.id)
+      }
+    }
+  )
+    .then(([rowsUpdate, [updatedGame]]) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(game));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error })
+    });
+});
+
+router.delete("/:id", function (req, res, next) {
+  Game.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(game => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(204).send(JSON.stringify(game));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error })
+    });
+});
+
 module.exports = router;
